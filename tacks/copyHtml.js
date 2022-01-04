e97b080bbf70088cafd575578ac50deb753a0bce
vano <alex@palya.ru>
2022-01-04 21:43:11 +0600

    html

--- tacks/copyHtml.js
+++ tacks/copyHtml.js
@@ -1,9 +1,12 @@
+const path = require('path')
 const {src,dest} = require('gulp');
 const browserSync = require('browser-sync');
+const fileinclude = require('gulp-file-include');
 const {DIST,SRC} = require('./../gulp.config');
 
 function copyHtml(){
     return src(`${SRC}/*.html`)
+        .pipe(fileinclude())
         .pipe(dest(DIST))
         .pipe(browserSync.stream());
 }
